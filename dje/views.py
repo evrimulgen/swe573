fbd3ff5703c42fbd3d2d83a31456abffd67bb1d8
Caner Turkmen <caner@exa-tech.com>
2013-07-01 15:51:39 +0300

    removed .idea, added urllib example, MAY NOT BUILD

--- dje/views.py
+++ dje/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from random import randint
+from django.http import HttpRequest
+import urllib2, urllib
 
 def home(request):
     a = []
@@ -10,61 +13,41 @@ def home(request):
 
     return render_to_response('statshome.html', {'weeklist': a})
 
-def team(request,num):
+def team(request, num):
     from random import randint
     a =[]
     for x in range(1, 19):
         a.append(x)
+
+#    post_data = [('name','Gladys'),]     # a sequence of two element tuples
+#    result = urllib2.urlopen('http://example.com', urllib.urlencode(post_data))
+#    content = result.read()
+
+    post_data = '{"team_ids":["1"]}'
+    result = urllib2.urlopen('http://sentio.cloudapp.net:8080/api/GetTeamsInformation', post_data)
+    content = result.read()
+
     b = []
-    for x in range(1, 35):
-        match = randint(0,34)
-        min = randint(0,12432)
-        goal = randint(0,25)
-        assist = randint(0,16)
-        yellow = randint(0,8)
-        red = randint(0,4)
-        run = randint(0,11300)
-        b.append({'jersey': "%s"%x, 'name': 'Serhat Kurtulus', 'match': int(match), 'min': int(min), 'goal': int(goal), 'assist': int(assist), 'yellow': int(yellow), 'red': int(red), 'run': int(run)})
-    team =""
-    if int(num)==1:
-        team = "Beşiktaş"
-    elif int(num)==2:
-        team = "Bursaspor"
-    elif int(num)==3:
-        team = "Eskişehirspor"
-    elif int(num)==4:
-        team = "Fenerbahçe"
-    elif int(num)==5:
-        team = "Galatasaray"
-    elif int(num)==6:
-        team = "Gençlerbirliği"
-    elif int(num)==7:
-        team = "Gaziantepspor"
-    elif int(num)==8:
-        team = "İstanbul BB"
-    elif int(num)==9:
-        team = "KDÇ Karabükspor"
-    elif int(num)==13:
-        team = "Mersin İY"
-    elif int(num)==15:
-        team = "Orduspor"
-    elif int(num)==17:
-        team = "Sivasspor"
-    elif int(num)==18:
-        team = "Trabzonspor"
-    else:
-        team = "Sentiospor"
+#    for x in range(1, 35):
+#        match = randint(0,34)
+#        min = randint(0,12432)
+#        goal = randint(0,25)
+#        assist = randint(0,16)
+#        yellow = randint(0,8)
+#        red = randint(0,4)
+#        run = randint(0,11300)
+#    b.append({'jersey': "%s"%x, 'name': 'Serhat Kurtulus', 'match': int(match), 'min': int(min), 'goal': int(goal), 'assist': int(assist), 'yellow': int(yellow), 'red': int(red), 'run': int(run)})
 
 
     return render_to_response('statsteam.html', {'team_list': a, 'team':team, 'team_selected': int(num), 'player_list':b})
 
 
 def league(request):
-    from random import randint
     a =[]
     for x in range(1, 19):
         a.append(x)
     b = []
+
     for x in range(1, 35):
         match = randint(0,34)
         min = randint(0,12432)

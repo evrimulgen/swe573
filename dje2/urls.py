ca37d6101fb856ade46803846f78e7164c25fdfb
Serhat Kurtulus <serhat.kurtulus@exa-tech.com>
2013-07-29 18:34:54 +0300

    serhat degisiklikler 130729

--- dje2/urls.py
+++ dje2/urls.py
@@ -5,24 +5,27 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'dje.views.home', name='home'),
-    url(r'^team/(\d*)/$', 'dje.views.team'),
-    url(r'^team/$', 'dje.views.team'),
-    url(r'^league/$', 'dje.views.league'),
-    url(r'^before/$', 'dje.views.before'),
-    url(r'^center/$', 'dje.views.center'),
-    url(r'^summary/$', 'dje.views.summary'),
-    url(r'^player/$', 'dje.views.player'),
-    url(r'^table/$', 'dje.views.table'),
-    url(r'^board/$', 'dje.views.chalkboard'),
-    url(r'^api/(?P<path>\w+)$', 'dje.views.router'),
+                       # Examples:
+                       url(r'^$', 'dje.views.home', name='home'),
+                       url(r'^team/(\d*)/$', 'dje.views.team'),
+                       url(r'^team/$', 'dje.views.team2'),
+                       url(r'^player/(\d*)/$', 'dje.views.player3'),
+                       url(r'^player/(?P<num>\d*)/(?P<player_id>\d*)/$', 'dje.views.playerx'),
+                       url(r'^league/$', 'dje.views.league'),
+                       url(r'^before/$', 'dje.views.before'),
+                       url(r'^center/$', 'dje.views.center'),
+                       url(r'^compare/$', 'dje.views.compare'),
+                       url(r'^summary/$', 'dje.views.summary'),
+                       url(r'^player/$', 'dje.views.player'),
+                       url(r'^table/$', 'dje.views.table'),
+                       url(r'^board/$', 'dje.views.chalkboard'),
+                       url(r'^api/(?P<path>\w+)$', 'dje.views.router'),
 
-    # url(r'^dje2/', include('dje2.foo.urls')),
+                       # url(r'^dje2/', include('dje2.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       # url(r'^admin/', include(admin.site.urls)),
 )

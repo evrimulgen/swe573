4fbf78f13a04f2ed91cf5653bbeefaaadcc3bf9a
Caner Turkmen <caner@exa-tech.com>
2013-08-14 15:49:30 +0300

    trying to fix UnicodeDecodeError at homepage

--- dje/utils.py
+++ dje/utils.py
@@ -12,28 +12,3 @@ def service_request(id, data):
         d = json.dumps(data)
         result = urllib2.urlopen(url,d)
     return result.read()
-
-
-def try_service():
-    data = {"leagueId":1,"seasonId":8918,"matchId":11730060}
-    teams = service_request("GetMatchNarration", data)
-    j_obj = json.loads(teams)
-    datalist = j_obj["data"]
-    narrationDict = []
-    for min in datalist:
-        if min == "_id":
-            print min
-        else:
-            x = "team"
-            team_id = datalist[min][x]
-            x = "typeInt"
-            type_ = datalist[min][x]
-            x = "text"
-            text_ = datalist[min][x]
-            print text_
-
-
-
-
-
-try_service()
